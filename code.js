3d7b2baed6dad3352f6f66432a5578e597234d6c
Dustybluelight <104851317+Dustybluelight@users.noreply.github.com>
2022-06-24 10:54:58 +0800

    changed font size and added highlighting over tasks

--- code.js
+++ code.js
@@ -26,20 +26,20 @@ function tloadList() {
 
 function addItem() {
     var newItem = document.createElement("div");
-    newItem.innerHTML = document.getElementById("box").value;
+    newItem.innerHTML = document.getElementById("wbox").value;
     newItem.onclick = removeItem;
-    document.getElementById("list").appendChild(newItem);
+    document.getElementById("wlist").appendChild(newItem);
     saveList();
 }
 function removeItem() {
-    document.getElementById("list").removeChild(this);
+    document.getElementById("wlist").removeChild(this);
     saveList();
 }
 function saveList() {
-    localStorage.storedList = document.getElementById("list").innerHTML;
+    localStorage.storedList = document.getElementById("wlist").innerHTML;
 }
 function loadList() {
-    document.getElementById("list").innerHTML = localStorage.storedList;
+    document.getElementById("wlist").innerHTML = localStorage.storedList;
     for (var i = 0; i < list.children.length; i++) {
         list.children[i].onclick = removeItem;
     }
