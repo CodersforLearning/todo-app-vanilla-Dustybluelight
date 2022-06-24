bfc8d573fe984e8d8f6fb008aef214cedda57db7
Dustybluelight <104851317+Dustybluelight@users.noreply.github.com>
2022-06-24 10:36:06 +0800

    got todo today working, gotta do todo week

--- code.js
+++ code.js
@@ -1,43 +1,50 @@
-class Todo {
-    addItem() {
-        var newItem = document.createElement("div");
-        newItem.innerHTML = document.getElementById("box").value;
-        newItem.onclick = removeItem;
-        document.getElementById("list").appendChild(newItem);
-        saveList();
-    }
-    removeItem() {
-        document.getElementById("list").removeChild(this);
-        saveList();
-    }
-    saveList() {
-        localStorage.storedList = document.getElementById("list").innerHTML;
-    }
-    loadList() {
-        document.getElementById("list").innerHTML = localStorage.storedList;
-        for (var i = 0; i < list.children.length; i++) {
-            list.children[i].onclick = removeItem;
-        }
-
-        if (localStorage.storedList) {
-            loadList();
-        }
+function taddItem() {
+    var newItem = document.createElement("div");
+    newItem.innerHTML = document.getElementById("box").value;
+    newItem.onclick = tremoveItem;
+    document.getElementById("tlist").appendChild(newItem);
+    tsaveList();
+}
+function tremoveItem() {
+    document.getElementById("tlist").removeChild(this);
+    saveList();
+}
+function tsaveList() {
+    localStorage.storedList = document.getElementById("tlist").innerHTML;
+}
+function tloadList() {
+    document.getElementById("tlist").innerHTML = localStorage.storedList;
+    for (var i = 0; i < list.children.length; i++) {
+        list.children[i].onclick = removeItem;
     }
 
+    if (localStorage.storedList) {
+        loadList();
+    }
 }
 
-let tododay = new Todo;
 
-function todoadd() {
-    tododay.addItem();
+function addItem() {
+    var newItem = document.createElement("div");
+    newItem.innerHTML = document.getElementById("box").value;
+    newItem.onclick = removeItem;
+    document.getElementById("list").appendChild(newItem);
+    saveList();
 }
+function removeItem() {
+    document.getElementById("list").removeChild(this);
+    saveList();
+}
+function saveList() {
+    localStorage.storedList = document.getElementById("list").innerHTML;
+}
+function loadList() {
+    document.getElementById("list").innerHTML = localStorage.storedList;
+    for (var i = 0; i < list.children.length; i++) {
+        list.children[i].onclick = removeItem;
+    }
 
-create();
-
-
-
-
-
-
-
-
+    if (localStorage.storedList) {
+        loadList();
+    }
+}
